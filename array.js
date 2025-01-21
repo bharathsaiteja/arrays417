e86f24697a9252eb5e0908c40a0e8e4eb3931d7e
bharathsaiteja <n.bsteja@gmail.com>
2025-01-21 11:07:02 +0530

    message

--- array.js
+++ array.js
@@ -12,3 +12,16 @@ let x = users.forEach((user,i)=>{
 }
 );
 console.log(x);
+
+let usersData={
+    name:"bharath",
+    age:20,
+    city:"khammam",
+};
+console.log(usersData.name, typeof(usersData));
+
+let n = JSON.stringify(usersData);
+console.log(x, typeof(x));
+
+let y = JSON.parse(x);
+console.log(y);
